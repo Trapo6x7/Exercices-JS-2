16034d940b53db71f01f894b57629f8ccf073ac1
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-18 14:36:18 +0100

    EXO4

---
+++ Exercice_4/script.js
@@ -0,0 +1,12 @@
+const name = document.querySelector("#lastname")
+const firstname = document.querySelector("#firstname")
+const city= document.querySelector("#city")
+let raz = document.querySelector("#raz");
+
+raz.addEventListener("click", handleRemoveValues);
+
+function handleRemoveValues(){
+    name.value = "";
+    firstname.value = "";
+    city.value = "";
+};
