844823c06ebb8ce14f4b45bc7acc2476a021fd5d
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-18 12:27:29 +0100

    EXO1 DOM

--- Exercice_1/script.js
+++ Exercice_1/script.js
@@ -1,12 +1,12 @@
-const img = document.querySelector("#image1")
+const img = document.querySelector("#image1");
 
 img.addEventListener("mouseover", handleImageChange);
 img.addEventListener("mouseout", handleImageChange2);
 
 function handleImageChange(){
     img.src="./images/image1_2.jpg"
-}
+};
 
 function handleImageChange2(){
     img.src="./images/image1.jpg"
-}
+};
